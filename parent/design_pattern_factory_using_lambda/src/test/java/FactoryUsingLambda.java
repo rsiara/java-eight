9251e148f7fe35694c0b341491dd99df2283e089
robert <siara.robert@gmail.com>
2019-03-04 21:22:39 +0100

    module: design_pattern_factory_using_lambda

---
+++ parent/design_pattern_factory_using_lambda/src/test/java/FactoryUsingLambda.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+@RunWith(SpringRunner.class)
+public class FactoryUsingLambda {
+
+    /*The factory design pattern lets you create objects without exposing the instantiation logic to the
+    client. For example, let’s say you’re working for a bank and they need a way of creating different
+    financial products: loans, bonds, stocks, and so on.
+
+    Typically you’d create a Factory class with a method that’s responsible for the creation of
+    different objects, as shown here:*/
+
+    static class CollectionFactory {
+        static List createList(String name) {
+            switch (name) {
+                case "ArrayList":
+                    return new ArrayList<>();
+                case "Vector":
+                    return new Vector();
+                case "LinkedList":
+                    return new LinkedList();
+                default:
+                    throw new RuntimeException("No such collection " + name);
+            }
+        }
+    }
+
+    @Test
+    public void factory_usage_using_lambda() {
+
+        List<String> list = CollectionFactory.createList("ArrayList");
+        list = CollectionFactory.createList("Vector");
+        list = CollectionFactory.createList("LinkedList");
+    }
+}
